42ab8fdba3a6072d76ae2e220fa4e0fee34ed8d0
bhaskar <bhaskartejaoffl@gmail.com>
2026-07-07 00:14:04 +0530

    DevOps: added conditional package exports mapping imports to source ts and requires to compiled CommonJS dist outputs to resolve Rollup static resolution bugs on Vercel

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,2 +1,18 @@
-export * from './types';
-export * from './ot/transform';
+export {
+  SOCKET_EVENTS,
+  type Operation,
+  type InsertOp,
+  type DeleteOp,
+  type RetainOp,
+  type VersionedOperation,
+  type OperationAck,
+  type PresenceUser,
+  type CursorPosition,
+  type AuthTokenPayload,
+} from './types';
+
+export {
+  applyOp,
+  transformOp,
+  transformAgainstHistory,
+} from './ot/transform';
